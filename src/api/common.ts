4cbc603cef77fb96693efa82201ec1b9a08bcb97
Soldier_RMB <soldierrmb0510@outlook.com>
2024-03-30 21:37:22 +0800

    Move some methods to common

---
+++ src/api/common.ts
@@ -0,0 +1,17 @@
+import request from '@/utils/request';
+
+export const apiGetProvinces = async () => {
+    return await request.get('/common/getProvinces');
+};
+
+export const apiGetCitiesByProvinceCode = async (provinceCode: number) => {
+    return await request.get('/common/getCitiesByProvinceCode', { params: { provinceCode } });
+};
+
+export const apiGetAreasByCityCode = async (cityCode: number) => {
+    return await request.get('/common/apiGetAreasByCityCode', { params: { cityCode } });
+};
+
+export const apiGetStreetByCityCode = async (streetCode: number) => {
+    return await request.get('/common/apiGetStreetByCityCode', { params: { streetCode } });
+};
--- src/api/guest.ts
+++ src/api/guest.ts
@@ -3,11 +3,3 @@ import request from '@/utils/request';
 export const apiGetAttractions = async () => {
     return await request.get('/guest/getAttractions');
 };
-
-export const apiGetProvinces = async () => {
-    return await request.get('/guest/getProvinces');
-};
-
-export const apiGetCitiesByProvinceCode = async (provinceCode: number) => {
-    return await request.get('/guest/getCitiesByProvinceCode', { params: { provinceCode } });
-};
