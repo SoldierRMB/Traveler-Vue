8b48a4d2aa774dc137b479b7d578df92a2bf88ec
Soldier_RMB <soldierrmb0510@outlook.com>
2024-03-30 23:39:58 +0800

    Add apiGetUserIsPresent

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,3 +3,7 @@ import request from '@/utils/request';
 export const apiLogin = async (data: any) => {
     return await request.post('/user/login', data);
 };
+
+export const apiGetUserIsPresent = async (username: string) => {
+    return await request.get('/user/getUserIsPresent', { params: { username } });
+};
