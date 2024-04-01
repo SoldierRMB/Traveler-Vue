f42f155eb72c89fc4fb7c23782908f9959336e24
Soldier_RMB <soldierrmb0510@outlook.com>
2024-04-01 13:37:49 +0800

    Add update and delete attraction

--- src/api/staff.ts
+++ src/api/staff.ts
@@ -1,10 +1,22 @@
 import request from '@/utils/request';
 import type { AttractionVO } from '@/types/interfaces';
 
+export const apiUpdateAttraction = async (data: AttractionVO, username: string) => {
+    return await request.put('/staff/updateAttraction', data, { params: { username: username } });
+};
+
+export const apiDeleteAttraction = async (attractionId: number, username: string) => {
+    return await request.put('/staff/deleteAttraction', null, {
+        params: { attractionId: attractionId, username: username }
+    });
+};
+
 export const apiPostAttraction = async (data: AttractionVO, username: string) => {
     return await request.post('/staff/postAttraction', data, { params: { username: username } });
 };
 
 export const apiGetAttractionsByUsername = async (username: string) => {
-    return await request.get('/staff/getUserAttractionsByUsername', { params: { username: username } });
+    return await request.get('/staff/getUserAttractionsByUsername', {
+        params: { username: username }
+    });
 };
