97782c16ca7a93e7965d9b9f2d2dce2d90e2c71e
Soldier_RMB <soldierrmb0510@outlook.com>
2024-04-01 13:34:55 +0800

    Custom response

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,11 +1,16 @@
 import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
-import router from '@/router';
 import { ElMessage } from 'element-plus';
 import { useAuthStore } from '@/stores/auth';
 const axiosInstance: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_TRAVELER_BASE_URL,
     timeout: 5000
 });
+interface Response<T = any> {
+    flag: boolean;
+    code: number;
+    message: string;
+    data: T;
+}
 const whitelist = ['/user', '/common'];
 //添加请求拦截器
 axiosInstance.interceptors.request.use(
@@ -26,9 +31,15 @@ axiosInstance.interceptors.request.use(
 );
 // 添加响应拦截器
 axiosInstance.interceptors.response.use(
-    (response: AxiosResponse) => {
+    (response: AxiosResponse<Response>) => {
         // 对响应数据做点什么
-        return response.data;
+        const data: Response = response.data;
+        return {
+            ...response,
+            data: data.data,
+            status: data.code,
+            statusText: data.message
+        };
     },
     (error: any) => {
         // 处理响应错误
