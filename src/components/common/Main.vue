b08f296b4034800d70a79f88bcca7b4ca9c6ecc4
Soldier_RMB <soldierrmb0510@outlook.com>
2024-04-06 11:43:48 +0800

    Tab to menu

--- src/components/common/Main.vue
+++ src/components/common/Main.vue
@@ -1,20 +1,18 @@
 <template>
-    <el-tabs v-model="activeName" class="tabs">
-        <el-tab-pane name="1" tab="1" label="旅游动态">
-            <Post></Post>
-        </el-tab-pane>
-        <el-tab-pane name="2" tab="2" label="旅游订票">
-            <BookingList></BookingList>
-        </el-tab-pane>
-    </el-tabs>
+    <div class="menu">
+        <el-menu :default-active="$route.path" mode="horizontal" router>
+            <el-menu-item index="/post">旅游动态</el-menu-item>
+            <el-menu-item index="/booking">景点订票</el-menu-item>
+        </el-menu>
+        <RouterView />
+    </div>
 </template>
 
 <script setup lang="ts">
-const activeName = ref('2');
 </script>
 
 <style scoped lang="scss">
-.tabs {
+.menu {
     display: flex;
     flex-direction: column;
     width: 100%;
@@ -22,19 +20,19 @@ const activeName = ref('2');
 }
 
 @media screen and (max-width: 1200px) {
-    .tabs {
+    .menu {
         padding: 0 10rem;
     }
 }
 
 @media screen and (max-width: 768px) {
-    .tabs {
+    .menu {
         padding: 0 5rem;
     }
 }
 
 @media screen and (max-width: 480px) {
-    .tabs {
+    .menu {
         padding: 0;
     }
 }
