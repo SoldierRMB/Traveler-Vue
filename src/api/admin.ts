ca792ab7988c38c197078734e2922964634cf041
Soldier_RMB <soldierrmb0510@outlook.com>
2024-05-06 20:19:39 +0800

    Add apiGetComments, apiCompleteDeleteComment, apiPublishAnnouncement, apiUpdateAnnouncement, apiCompleteDeleteAnnouncement

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -34,4 +34,30 @@ export const apiCompleteDeletePost = async (postId: number) => {
     return await request.delete('/admin/completeDeletePost', {
         params: { postId }
     });
+};
+
+export const apiGetComments = async (current: number, size: number) => {
+    return await request.get('/admin/getComments', {
+        params: { current, size }
+    });
+};
+
+export const apiCompleteDeleteComment = async (commentId: number) => {
+    return await request.delete('/admin/completeDeleteComment', {
+        params: { commentId }
+    });
+};
+
+export const apiPublishAnnouncement = async (data: any) => {
+    return await request.post('/admin/publishAnnouncement', data);
+};
+
+export const apiUpdateAnnouncement = async (data: any) => {
+    return await request.put('/admin/updateAnnouncement', data);
+};
+
+export const apiCompleteDeleteAnnouncement = async (announcementId: number) => {
+    return await request.delete('/admin/completeDeleteAnnouncement', {
+        params: { announcementId }
+    });
 }
