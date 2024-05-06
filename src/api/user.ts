1705489e7070ca1828e24102e6a58cef25ea7b45
Soldier_RMB <soldierrmb0510@outlook.com>
2024-05-06 20:22:38 +0800

    Add apiSendCode, apiRegister, apiChangePassword

--- src/api/user.ts
+++ src/api/user.ts
@@ -7,3 +7,15 @@ export const apiLogin = async (data: any) => {
 export const apiGetUserIsPresent = async (username: string) => {
     return await request.get('/user/getUserIsPresent', { params: { username } });
 };
+
+export const apiSendCode = async (data: any) => {
+    return await request.post('/user/sendCode', data);
+};
+
+export const apiRegister = async (data: any) => {
+    return await request.post('/user/register', data);
+};
+
+export const apiChangePassword = async (data: any, username: string) => {
+    return await request.put('/user/changePassword', data, { params: { username } });
+};
