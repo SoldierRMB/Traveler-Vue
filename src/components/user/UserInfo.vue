0d150dd3ed49e08488b7b4da38fe4edce4993985
Soldier_RMB <soldierrmb0510@outlook.com>
2024-05-06 20:28:20 +0800

    Add change password

--- src/components/user/UserInfo.vue
+++ src/components/user/UserInfo.vue
@@ -4,7 +4,7 @@
         :model="form"
         :rules="rules"
         class="form"
-        label-width="80px"
+        label-width="auto"
         status-icon
         @submit.prevent
     >
@@ -12,16 +12,19 @@
             <el-form-item label="用户名" prop="username">
                 <el-input placeholder="请输入密码" v-model="form.username" disabled />
             </el-form-item>
-            <el-form-item label="密码" prop="password">
-                <el-input placeholder="请输入密码" v-model="form.password" />
+            <el-form-item label="旧密码" prop="oldPassword">
+                <el-input placeholder="请输入旧密码" type="password" v-model="form.oldPassword" />
             </el-form-item>
-            <el-form-item label="确认密码" prop="confirmPassword">
-                <el-input placeholder="请输入再次输入密码" v-model="form.confirmPassword" />
+            <el-form-item label="新密码" prop="newPassword">
+                <el-input placeholder="请输入新密码" type="password" v-model="form.newPassword" />
             </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="changePassword(formRef)">提交</el-button>
+            <el-form-item label="确认新密码" prop="confirmNewPassword">
+                <el-input placeholder="请输入再次输入新密码" type="password" v-model="form.confirmNewPassword" />
             </el-form-item>
         </div>
+        <el-button type="primary" class="button" @click="changePassword(formRef)">
+            修改密码
+        </el-button>
     </el-form>
 </template>
 
@@ -29,38 +32,27 @@
 import type { FormInstance, FormRules } from 'element-plus';
 import type { PasswordVO } from '@/types/interfaces';
 import { useAuthStore } from '@/stores/auth';
+import { apiChangePassword } from '@/api/user';
 
 const formRef = ref<FormInstance>();
 const form = ref({} as PasswordVO);
 const authStore = useAuthStore();
 const username = authStore.user?.sub;
 
-const validatePassword = (rule: any, value: any, callback: any) => {
+const validateConfirmNewPassword = (rule: any, value: any, callback: any) => {
     if (!value) {
-        callback(new Error('请输入密码'));
-    } else {
-        if (form.value.password !== '') {
-            if (!formRef.value) return;
-            formRef.value.validateField('password', () => null);
-        }
-        callback();
-    }
-};
-
-const validateConfirmPassword = (rule: any, value: any, callback: any) => {
-    if (!value) {
-        callback(new Error('请再次输入密码'));
-    } else if (value !== form.value.password) {
+        callback(new Error('请再次输入新密码'));
+    } else if (value !== form.value.newPassword) {
         callback(new Error('两次输入密码不一致!'));
-    } else {
-        callback();
     }
+    callback();
 };
 
 const rules = reactive<FormRules<PasswordVO>>({
     username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
-    password: [{ required: true, validator: validatePassword, trigger: 'blur' }],
-    confirmPassword: [{ required: true, validator: validateConfirmPassword, trigger: 'blur' }]
+    oldPassword: [{ required: true, message: '请输入旧密码', trigger: 'blur' }],
+    newPassword: [{ required: true, message: '请输入新密码', trigger: 'blur' }],
+    confirmNewPassword: [{ required: true, validator: validateConfirmNewPassword, trigger: 'blur' }]
 });
 
 onMounted(() => {
@@ -71,10 +63,12 @@ const changePassword = async (formEl: FormInstance | undefined) => {
     if (!formEl) return;
     await formEl.validate(async (valid, fields) => {
         if (valid) {
-            console.log('submit!');
-        } else {
-            console.log('error submit!');
-            return false;
+            const res = await apiChangePassword(form.value, username as string);
+            if (res.status === 200) {
+                ElMessage.success('密码修改成功');
+            } else {
+                ElMessage.error('密码不匹配');
+            }
         }
     });
 };
@@ -87,8 +81,12 @@ const changePassword = async (formEl: FormInstance | undefined) => {
     align-items: center;
 
     .formItems {
-        width: 50rem;
-        transform: translateX(-15%);
+        width: 60%;
+        transform: translateX(-6%);
+
+        .button {
+            text-align: end;
+        }
     }
 }
 </style>
