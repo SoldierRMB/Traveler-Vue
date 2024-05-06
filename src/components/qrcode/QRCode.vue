da69bdb45f6d18e134657033ad48ad12e539bece
Soldier_RMB <soldierrmb0510@outlook.com>
2024-05-06 20:29:32 +0800

    Add qr code

---
+++ src/components/qrcode/QRCode.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="qrCode">
+        <QrcodeVue
+            :size="250"
+            :value="props.value"
+            :foreground="'#64c864'"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import QrcodeVue from 'qrcode.vue'
+
+const props = defineProps({
+    value: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+.qrCode {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+}
+</style>
+
