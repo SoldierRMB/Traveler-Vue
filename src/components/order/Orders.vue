eb474bdb971a29db45d700c2815b486d7c54e743
Soldier_RMB <soldierrmb0510@outlook.com>
2024-05-06 20:34:19 +0800

    Add rating dialog

--- src/components/order/Orders.vue
+++ src/components/order/Orders.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="ordersBox">
         <div>
-            <div class="filter">
+            <div class="filter" v-if="false">
                 <el-input v-model="keyword" placeholder="请输入订单编号" class="search" />
                 <el-button type="primary" @click="searchOrders" class="searchButton"
                     >搜索</el-button
@@ -20,20 +20,22 @@
                         <el-tag type="warning" v-if="scope.row.status === 1">待支付</el-tag>
                         <el-tag type="success" v-if="scope.row.status === 2">已支付</el-tag>
                         <el-tag type="success" v-if="scope.row.status === 3">已完成</el-tag>
-                        <el-tag type="info" v-if="scope.row.status === 4">已取消</el-tag>
+                        <el-tag type="success" v-if="scope.row.status === 4">已评价</el-tag>
+                        <el-tag type="info" v-if="scope.row.status === 5">已取消</el-tag>
+                        <el-tag type="danger" v-if="scope.row.status === 6">已删除</el-tag>
                     </template>
                 </el-table-column>
                 <el-table-column
                     align="center"
                     label="门票数量(张)"
                     prop="quantity"
-                    min-width="8rem"
+                    min-width="6rem"
                 />
                 <el-table-column
                     align="center"
                     label="订单金额(元)"
                     prop="amount"
-                    min-width="8rem"
+                    min-width="6rem"
                 />
                 <el-table-column
                     align="center"
@@ -44,7 +46,7 @@
                 <el-table-column
                     align="center"
                     label="操作"
-                    min-width="8rem"
+                    min-width="10rem"
                     v-if="userRoleRef === 'ROLE_TOURIST'"
                 >
                     <template #default="scope">
@@ -57,7 +59,15 @@
                                 >支付订单</el-button
                             >
                             <el-button type="primary" size="small" v-if="scope.row.status === 2"
+                            @click="openQRCodeDialog(scope.row)"
                                 >查看门票</el-button
+                            >
+                            <el-button
+                                type="primary"
+                                size="small"
+                                v-if="scope.row.status === 3"
+                                @click="openRatingDialog(scope.row)"
+                                >评价景点</el-button
                             >
                             <el-popconfirm
                                 title="确认取消订单吗？"
@@ -67,20 +77,24 @@
                                 @confirm="cancelOrder(scope.row.id)"
                             >
                                 <template #reference>
-                                    <el-button
-                                        type="danger"
-                                        size="small"
-                                        v-if="scope.row.status === 1 || scope.row.status === 2"
-                                        >取消订单</el-button
-                                    >
+                                    <el-button type="danger" size="small">取消订单</el-button>
                                 </template>
                             </el-popconfirm>
-                            <el-button
-                                type="danger"
-                                size="small"
-                                v-if="scope.row.status === 3 || scope.row.status === 4"
-                                >删除订单</el-button
+                            <el-popconfirm
+                                title="确认删除订单吗？"
+                                confirm-button-text="确认"
+                                cancel-button-text="取消"
+                                v-if="
+                                    scope.row.status === 3 ||
+                                    scope.row.status === 4 ||
+                                    scope.row.status === 5
+                                "
+                                @confirm="deleteOrder(scope.row.id)"
                             >
+                                <template #reference>
+                                    <el-button type="danger" size="small">删除订单</el-button>
+                                </template>
+                            </el-popconfirm>
                         </div>
                     </template>
                 </el-table-column>
@@ -95,8 +109,24 @@
             class="pagination"
         />
     </div>
-    <el-dialog title="订单支付" width="30rem" v-model="paymentDialogVisible">
+    <el-dialog
+        title="订单支付"
+        width="30rem"
+        v-model="paymentDialogVisible"
+        @close="paymentDialogVisible = false"
+    >
         <Payment :orderId="orderId" />
+    </el-dialog>
+    <el-dialog title="查看门票" width="30rem" v-model="qrCodeDialogVisible" @close="qrCodeDialogVisible = false">
+        <QRCode :value="String(orderId)"/>
+    </el-dialog>
+    <el-dialog
+        title="评价景点"
+        width="60%"
+        v-model="ratingDialogVisible"
+        @close="ratingDialogVisible = false"
+    >
+        <RatingForm :orderId="orderId" @closeDialog="closeDialog" />
     </el-dialog>
 </template>
 
@@ -105,7 +135,7 @@ import moment from 'moment';
 import { useAuthStore } from '@/stores/auth';
 import { apiGetAllOrders } from '@/api/admin';
 import { apiGetOrdersByAttractionId, apiUseTicket } from '@/api/staff';
-import { apiGetUserOrders, apiCancelOrder } from '@/api/tourist';
+import { apiGetUserOrders, apiCancelOrder, apiDeleteOrder } from '@/api/tourist';
 import { useRoute } from 'vue-router';
 
 const authStore = useAuthStore();
@@ -175,6 +205,14 @@ const cancelOrder = async (orderId: string) => {
     });
 };
 
+const deleteOrder = async (orderId: string) => {
+    await apiDeleteOrder(orderId, username).then((res) => {
+        if (res.status === 200) {
+            getOrders();
+        }
+    });
+};
+
 const handleRowClick = async (row: any, column: any, event: Event) => {
     if (userRoleRef.value === 'ROLE_STAFF' && row.status === 2) {
         ElMessageBox.confirm('使用此门票吗？', '使用门票', {
@@ -182,7 +220,7 @@ const handleRowClick = async (row: any, column: any, event: Event) => {
             cancelButtonText: '取消',
             type: 'warning'
         }).then(async () => {
-            const res = await apiUseTicket(attractionId, row.id, username);
+            const res = await apiUseTicket(row.id, username);
             if (res.status === 200) {
                 ElMessage.success('门票使用成功');
             } else {
@@ -191,6 +229,24 @@ const handleRowClick = async (row: any, column: any, event: Event) => {
             getOrders();
         });
     }
+};
+
+const ratingDialogVisible = ref(false);
+
+const openRatingDialog = (row: any) => {
+    ratingDialogVisible.value = true;
+    orderId.value = row.id;
+};
+
+const qrCodeDialogVisible = ref(false);
+
+const openQRCodeDialog = (row: any) => {
+    qrCodeDialogVisible.value = true;
+    orderId.value = row.id;
+}
+
+const closeDialog = (val: boolean) => {
+    ratingDialogVisible.value = val;
 };
 
 const keyword = ref('');
